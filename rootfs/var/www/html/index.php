34051c923a5c3bcdf064ad743f1ee7e18e75f84a
jschwind <info@juergen-schwind.de>
2024-10-28 10:53:02 +0100

    Update index.php
    Created a new Adminer index.php with default environment variable values for server, username, password, and database. This enhances user experience by pre-filling login forms, leveraging environment settings.

--- rootfs/var/www/html/index.php
+++ rootfs/var/www/html/index.php
@@ -1,50 +1,73 @@
 <?php
 
 namespace docker {
-	function adminer_object() {
-		require_once('plugins/plugin.php');
+    function adminer_object() {
+        require_once('plugins/plugin.php');
 
-		class Adminer extends \AdminerPlugin {
-			function _callParent($function, $args) {
-				if ($function === 'loginForm') {
-					ob_start();
-					$return = \Adminer::loginForm();
-					$form = ob_get_clean();
+        class Adminer extends \AdminerPlugin {
+            function _callParent($function, $args) {
+                if ($function === 'loginForm') {
+                    ob_start();
+                    $return = \Adminer::loginForm();
+                    $form = ob_get_clean();
 
-					echo str_replace('name="auth[server]" value="" title="hostname[:port]"', 'name="auth[server]" value="'.(getenv('ADMINER_DEFAULT_SERVER') ?: 'db').'" title="hostname[:port]"', $form);
+                    $form = str_replace(
+                        'name="auth[server]" value="" title="hostname[:port]"',
+                        'name="auth[server]" value="'.(getenv('ADMINER_DEFAULT_SERVER') ?: 'db').'" title="hostname[:port]"',
+                        $form
+                    );
 
-					return $return;
-				}
+                    $form = str_replace(
+                        'name="auth[username]" id="username" value=""',
+                        'name="auth[username]" value="'.(getenv('ADMINER_DEFAULT_USER') ?: '').'"',
+                        $form
+                    );
 
-				return parent::_callParent($function, $args);
-			}
-		}
+                    $form = str_replace(
+                        'name="auth[password]"',
+                        'name="auth[password]" value="'.(getenv('ADMINER_DEFAULT_PASSWORD') ?: '').'"',
+                        $form
+                    );
 
-		$plugins = [];
-		foreach (glob('plugins-enabled/*.php') as $plugin) {
-			$plugins[] = require($plugin);
-		}
+                    $form = str_replace(
+                        'name="auth[db]"',
+                        'name="auth[db]" value="'.(getenv('ADMINER_DEFAULT_DB') ?: '').'"',
+                        $form
+                    );
 
-		if (is_dir('plugins-custom')) {
-			foreach (glob('plugins-custom/*.php') as $plugin) {
-				$plugins[] = require($plugin);
-			}
-		}
+                    echo $form;
+                    return $return;
+                }
 
-		return new Adminer($plugins);
-	}
+                return parent::_callParent($function, $args);
+            }
+        }
+
+        $plugins = [];
+        foreach (glob('plugins-enabled/*.php') as $plugin) {
+            $plugins[] = require($plugin);
+        }
+
+        if (is_dir('plugins-custom')) {
+            foreach (glob('plugins-custom/*.php') as $plugin) {
+                $plugins[] = require($plugin);
+            }
+        }
+
+        return new Adminer($plugins);
+    }
 }
 
 namespace {
-	if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
-		header('Content-Type: text/css');
-		readfile('adminer.css');
-		exit;
-	}
+    if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+        header('Content-Type: text/css');
+        readfile('adminer.css');
+        exit;
+    }
 
-	function adminer_object() {
-		return \docker\adminer_object();
-	}
+    function adminer_object() {
+        return \docker\adminer_object();
+    }
 
-	require('adminer.php');
+    require('adminer.php');
 }
